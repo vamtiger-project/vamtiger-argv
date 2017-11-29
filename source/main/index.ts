d6815f2ed82760d8063ecfa1428bbd74913ba940
James-Alex Matthews <vamtiger@vamtiger.com>
2017-11-29 03:11:37 +0200

    vamtiger-node-typescript-commit

---
+++ source/main/index.ts
@@ -0,0 +1,32 @@
+import { default as rawArguments, set as rawArgumentsSet, RawArgument, RawArguments } from '../raw-arguments';
+import initialize from '../initialize';
+import getNextArgument from '../get-next-argument';
+import { IArgs, Index } from '../types';
+
+export default class Args extends Map<RawArgument, RawArgument|undefined> implements IArgs {
+    first: RawArgument|undefined;
+
+    constructor() {
+        super();
+
+        this.first = undefined;
+
+        this.raw.forEach((arg: RawArgument, index: Index, args: RawArguments) => this._initialize(arg, index, args));
+    }
+
+    get raw() {
+        return rawArguments;
+    }
+
+    next(arg: string) {
+        const args = this.raw;
+
+        return getNextArgument({arg, args});
+    }
+
+    _initialize(arg: RawArgument, index: Index, args: RawArguments) {
+        const context = this;
+
+        initialize({context, arg, index, args});
+    }
+};
